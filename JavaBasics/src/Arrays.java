621b414632c9fb51d62d1629852ceeb1a875eb44
Arjun gupta <rachnag352@gmail.com>
2025-07-26 23:42:15 +0530

    performed seaching and fucntions in an array!

--- JavaBasics/src/Arrays.java
+++ JavaBasics/src/Arrays.java
@@ -60,6 +60,7 @@ public class Arrays {
 
      //                             largest in an array!
 
+     /*
      import java.util.Scanner;
  public class Arrays {
  public static int searchLargest(int arr[]) {
@@ -83,3 +84,102 @@ public class Arrays {
         System.out.println("Larget value in the Array is --> "+largest);
     }
  }
+ */
+
+//                          binary seach
+ /*
+import java.util.Scanner;
+public class Arrays {
+public static int binarySearch(int array[],int key) {
+    int start=0;
+    int end=array.length-1;
+    while (start<=end) {
+        int mid =(start+end)/2;
+        if(array[mid]==key){
+            return mid;
+        }
+        if(array[mid]<key){//right
+            start=mid+1;
+        }
+        else{//left
+            end=mid-1;
+        }
+    }
+    return -1;
+}
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int array[]={2,4,6,8,10,12,14,16,18,20};
+        int key=10;
+        int value=binarySearch(array, key);
+        if(value!=-1){
+            System.out.println("Key found at position "+value);
+        }
+        else{
+            System.out.println("Key cannot be found in the array!");
+        }
+    }
+}
+     */
+
+    //                      Reverse an array
+/*
+    public class Arrays {
+    public static void reverse(int arr[]) {
+        int start=0;
+        int end=arr.length-1;
+        while(start<end){
+            int temp=arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp;
+            start++;
+            end--;
+        }
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+        public static void main(String[] args) {
+            int array[]={2,4,6,8,10,12,14,16,18,20};
+            reverse(array);
+        }
+    }
+         */
+
+        //  making pair's from an array:-
+/*
+public class Arrays {
+    public static void findPair(int arr[]) {
+        for(int i=0;i<arr.length-1;i++){
+            for(int j=i+1;j<arr.length;j++){
+                System.out.print("{"+arr[i]+","+arr[j]+"}");
+            }
+            System.err.println("");
+        }
+    }    
+    public static void main(String[] args) {
+        int array[]={2,4,6,8,10};
+        findPair(array); 
+    }
+}
+*/
+
+//                  printing subarrays!
+
+public class Arrays {
+public static void subarrays(int arr[]) {
+    for(int i=0;i<arr.length;i++){//0
+        for(int j=i;j<arr.length;j++){//0
+            for(int k=i;k<=j;k++){
+                System.out.print(arr[k]+" ");
+            }
+            System.out.println(" ");
+        }
+        System.out.println(" ");
+    }
+}
+    public static void main(String[] args) {
+      int array[]={2,4,6,8,10};
+      subarrays(array);  
+    }
+}
