ecfc5e873341f078179f10078d469e02a3ebb2c8
Arjun gupta <rachnag352@gmail.com>
2025-09-27 23:45:32 +0530

    done access modifiers, encapsulation and constructors

--- JavaBasics/src/Class.java
+++ JavaBasics/src/Class.java
@@ -1,10 +1,12 @@
 public class Class {
     public static void main(String[] args) {
-        Pen p1=new Pen();
-        p1.setAttr(0.5f, "Swiss Militery", "Gold");
-        p1.printPen();
+        BankAccount b1=new BankAccount();
+        b1.setAccountDetails(007, "Arjun gupta");
+        System.out.println(b1.getHolderName());
+        System.out.println(b1.getAccountNumber());
     }
 }
+/*
 class Pen {
 float tip;
 String name;
@@ -18,3 +20,38 @@ String colour;
         System.out.println(name+" "+"Colour "+colour+" Tip Size "+tip);
     }
 }
+ */
+
+// access specifiers in java 
+// there are 4 types of access specifiers in java:-
+// 1. Private 
+// 2. default 
+// 3. protected 
+// 4. public
+
+class BankAccount {
+    public String holderName;
+    private int accountNumber;
+    BankAccount(){
+        System.out.println("Constructor called!");
+    }
+    void setAccountDetails(int accountNumber,String holderName){
+        this.holderName=holderName;
+        this.accountNumber=accountNumber;
+    }    
+    int getAccountNumber(){
+        return this.accountNumber;
+    }
+    String getHolderName(){
+        return this.holderName;
+    }
+}
+// The four pillars of Object-Oriented Programming (OOP) are:-
+//  Encapsulation
+//  Abstraction
+//  Inheritance
+//  Polymorphism
+//  These principles provide a robust framework for developing modular, reusable, and maintainable code by enabling developers to model real-world scenarios effectively, leading to scalable and adaptable software.
+
+
+// Encapsulation: This involves bundling data (attributes) and the methods (functions) that operate on that data within a single unit, such as a class. It helps protect data by hiding its internal implementation and controlling access to it through public interfaces, preventing unintended modifications. 
