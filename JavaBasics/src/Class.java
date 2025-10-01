e9826190920cde0522e6a6cd94a5713be3334319
Arjun gupta <rachnag352@gmail.com>
2025-10-01 14:47:42 +0530

    performed inheritance

--- JavaBasics/src/Class.java
+++ JavaBasics/src/Class.java
@@ -1,14 +1,35 @@
 public class Class {
     public static void main(String[] args) {
-        BankAccount b1=new BankAccount();
-        BankAccount b2=new BankAccount(7751);
-        b1.setAccountDetails(007, "Arjun gupta");
-        System.out.println(b1.getHolderName());
-        System.out.println(b1.getAccountNumber());
-        System.out.println(b2.getPassword());
-    }
+        Dog kimmi=new Dog();
+        Cat cat=new Cat();
+        Human human=new Human();
+        Tuna tuna=new Tuna();
+        Shark shark=new Shark();
+        Peacaok peacaok=new Peacaok();
+        kimmi.legs=4;
+        kimmi.barks();
+        cat.legs=4;
+        cat.Meow();
+        human.legs=2;
+        human.speaks();
+        peacaok.fly=false;
+        peacaok.nationalBird();
+        tuna.canBeEaten=true;
+        tuna.tuna();
+        shark.canBeEaten=false;
+        shark.shark();
+        System.out.println("Mammels Legs:-");
+        System.out.println("Dog "+kimmi.legs);
+        System.out.println("Cat "+cat.legs);
+        System.out.println("Human "+human.legs);
+        System.out.println("Bird fly:-");
+        System.out.println("Peakock "+peacaok.fly);
+        System.out.println("Fish can be eaten:-");
+        System.out.println("Tuna "+tuna.canBeEaten);
+        System.out.println("Shark "+shark.canBeEaten);
+        }
 }
-/*
+
 class Pen {
 float tip;
 String name;
@@ -22,7 +43,7 @@ String colour;
         System.out.println(name+" "+"Colour "+colour+" Tip Size "+tip);
     }
 }
- */
+ 
 
 // access specifiers in java 
 // there are 4 types of access specifiers in java:-
@@ -74,3 +95,127 @@ class BankAccount {
 
 
 // Encapsulation: This involves bundling data (attributes) and the methods (functions) that operate on that data within a single unit, such as a class. It helps protect data by hiding its internal implementation and controlling access to it through public interfaces, preventing unintended modifications. 
+
+// Inheritance:Inheritance in Java is a core principle of Object-Oriented Programming (OOP) that allows a class to inherit properties (fields) and behaviors (methods) from another class. This mechanism promotes code reusability and establishes an "IS-A" relationship between classes, such as a "Dog IS-A Animal."
+
+// types of inheritance in java:
+// there are 4 types of inheritace in java
+
+// 1. Single level
+/*
+class Animal {
+    String colour;
+    void eat(){
+        System.out.println("Eats");
+    }
+    void breath(){
+        System.out.println("Breathes");
+    }
+    
+}
+class Dog extends Animal{
+    void bark(){
+        System.out.println("Barks");
+    }
+}
+     */ 
+// Multilevel Inheritance
+/*
+class Animal{
+    int colour;
+    void eat(){
+        System.out.println("Eats");
+    }
+}
+
+class mammel extends Animal{
+    int legs;
+}
+class Dog extends mammel{
+    boolean breads;
+}
+
+ */
+
+//3. Hierarchical Inheritance
+/*
+class Animal {
+    String colour;
+    void eats(){
+        System.out.println("Eats");
+    }
+    void breath(){
+        System.out.println("Breathes");
+    }
+}
+class Fish extends Animal{
+    void swims(){
+        System.out.println("Fish Swims in the water");
+    }
+}
+class Dog extends Animal{
+    void walks(){
+        System.out.println("Dogs can walk!");
+    }
+}
+     */
+
+
+// 4.Hybrid Inheritance
+
+class Animal{
+    String Colour;
+    void eats(){
+        System.out.println("Eats");
+    }
+    void breath(){
+        System.out.println("Breaths");
+    }
+}
+class Fish extends Animal{
+    boolean canBeEaten;
+    void swims(){
+        System.out.println("The fish can swims");
+    }
+}
+class Tuna extends Fish{
+    void tuna(){
+        System.out.println("Tuna can be eaten");
+    }
+}
+class Shark extends Fish{
+    void shark(){
+        System.out.println("Sharks cannot be eaten, Theyll eats us!");
+    }
+}
+
+class Bird extends Animal{
+    boolean fly;
+    void flying(){
+        System.out.println("A bird can fly");
+    }
+}
+class Peacaok extends Bird{
+    void nationalBird(){
+        System.out.println("Peacaok is our national animal");
+    }
+} 
+
+class Mammel extends Animal{
+    int legs;
+}
+class Dog extends Mammel{
+    void barks(){
+        System.out.println("Dogs Barks");
+    }
+}
+class Human extends Mammel{
+    void speaks(){
+        System.out.println("Human Speaks");
+    }
+}
+class Cat extends Mammel{
+    void Meow(){
+        System.out.println("Cat Meow");
+    }
+}
