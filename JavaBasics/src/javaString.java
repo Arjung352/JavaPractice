88852ebe5618b2f2d580fae7f0f15f4e20119dd5
Arjun gupta <rachnag352@gmail.com>
2025-10-24 22:18:19 +0530

    performed a string question

--- JavaBasics/src/javaString.java
+++ JavaBasics/src/javaString.java
@@ -9,6 +9,9 @@
     }
 }*/
 
+
+// checking wheather the string is palindrome or not
+/*
 public class javaString {
     public static boolean checkPalindrome(String str){
         int len=str.length();
@@ -25,3 +28,35 @@ public class javaString {
         System.out.println(checkPalindrome(str));
     }
 }
+     */
+
+    //  shortest path to reach the destination
+
+    public class javaString {
+    public static double shortestPath(String str) {
+        int len=str.length();
+        int x=0;
+        int y=0;
+        for(int i=0;i<len;i++){
+            if(str.charAt(i)=='w'){
+                x--;
+            }
+            else if(str.charAt(i)=='e'){
+                x++;
+            }
+            else if(str.charAt(i)=='n'){
+                y++;
+            }
+            else {
+                y--;
+            }
+        }
+        double minDistance=Math.sqrt((x*x)+(y*y));
+        return minDistance; 
+
+    }
+        public static void main(String[] args) {
+            String str=new String("wneenesennn");
+            System.out.println(shortestPath(str));            
+        }
+    }
