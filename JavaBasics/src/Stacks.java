544c6fa24906448fa6390e104d804ebd3f73f288
Arjun gupta <rachnag352@gmail.com>
2026-03-20 22:20:52 +0530

    solved max histogram area problem

--- JavaBasics/src/Stacks.java
+++ JavaBasics/src/Stacks.java
@@ -256,6 +256,7 @@ public class Stacks {
     }
          */
     // duplicate parantheses
+    /*
     public static boolean duplicateParantheses(String str) {
             Stack<Character>s=new Stack<>();
             int len=str.length();
@@ -281,6 +282,48 @@ public class Stacks {
             }
             return false;
 
+    }
+             */
+    public static void maxArea(int arr[]) {
+        int maxArea=0;
+        int nsr[]=new int[arr.length];
+        int nsl[]=new int[arr.length];
+
+        // next smaller right
+        Stack<Integer>s=new Stack<>();
+        for(int i=arr.length-1;i>=0;i--){
+            while(!s.isEmpty()&&arr[s.peek()]>=arr[i]){
+                s.pop();
+            }
+            if(s.isEmpty()){
+                nsr[i]=arr.length;
+            }
+            else{
+                nsr[i]=s.peek();
+            }
+            s.push(i);
+        }
+        // next smaller left
+        s=new Stack<>();
+        for(int i=0;i<arr.length;i++){
+            while(!s.isEmpty()&&arr[s.peek()]>=arr[i]){
+                s.pop();
+            }
+            if(s.isEmpty()){
+                nsl[i]=-1;
+            }
+            else{
+                nsl[i]=s.peek();
+            }
+            s.push(i);
+        }
+        for(int i=0;i<arr.length;i++){
+            int height=arr[i];
+            int width=nsr[i]-nsl[i]-1;
+            int currArea=height*width;
+            maxArea=Math.max(maxArea, currArea);
+        }
+        System.out.println("Max area ->"+maxArea);
     }
     public static void main(String[] args) {
     // Stacks outer = new Stacks();
@@ -324,9 +367,10 @@ public class Stacks {
         System.out.println(span[i]);
     }
          */
-        int arr[]={6,8,0,1,3};
+        int arr[]={2,1,5,6,2,3};
         // nextGreater(arr);
         // System.out.println(validParanthesis("([](){})"));
-        System.out.println(duplicateParantheses("(a+b)"));
+        // System.out.println(duplicateParantheses("(a+b)"));
+        maxArea(arr);
     }
 }
