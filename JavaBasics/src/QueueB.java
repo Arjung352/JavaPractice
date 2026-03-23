57b37b364b48d42ef13e60d75af0a3072613f9a2
Arjun gupta <rachnag352@gmail.com>
2026-03-23 09:20:09 +0530

    implemented Queue using arrays, linkedList and implemented circular queue as-well-as using jcf

---
+++ JavaBasics/src/QueueB.java
@@ -0,0 +1,181 @@
+import java.util.*;
+public class QueueB {
+    static class MyQueue{
+        static int arr[];
+        static int size;
+        static int rear;
+        MyQueue(int n){
+            arr=new int[n];
+            size=n;
+            rear=-1;
+        }
+        // isEmpty
+        public static boolean isEmpty(){
+            return rear==-1;
+        }
+        // add O(1)
+        public static void add(int data) {
+            if(rear==size-1){
+                System.out.println("Queue is full");
+                return;
+            }
+            rear=rear+1;
+            arr[rear]=data;
+        }
+        // remove O(N)
+        public static int remove() {
+            if(isEmpty()){
+                System.out.println("Queue already empty");
+                return -1;
+            }
+            int front=arr[0];
+            // shifting every element in the queue by 1 place to front to remove the front
+            for(int i=0;i<rear;i++){
+                arr[i]=arr[i+1];
+            }
+            rear=rear-1;
+            return front;
+        }
+        // peek O(1)
+        public static int peek() {
+            if(isEmpty()){
+                System.out.println("Queue is empty");
+                return -1;
+            }
+            return arr[0];
+        }
+    }
+    static class CircularQueue{
+        static int arr[];
+        static int size;
+        static int rear;
+        static int front;
+        CircularQueue(int n){
+            arr=new int[n];
+            size=n;
+            rear=-1;
+            front=-1;
+        }
+        // isEmpty
+        public static boolean isEmpty(){
+            return rear==-1&&front==-1;
+        }
+        public static boolean isFull(){
+             return (rear+1)%size==front; 
+        }
+        // add O(1)
+        public static void add(int data) {
+            if(isFull()){
+                System.out.println("Queue is full");
+                return;
+            }
+            // adding first element
+            if(front==-1){
+                front=0;
+            }
+            rear=(rear+1)%size;
+            arr[rear]=data;
+        }
+        // remove O(1)
+        public static int remove() {
+            if(isEmpty()){
+                System.out.println("Queue already empty");
+                return -1;
+            }
+            int result=arr[front];
+            // if there's only 1 element in the queue
+            if(front==rear){
+                front=rear=-1;
+            }
+            else{
+                front=(front+1)%size;
+            }
+            return result;
+        }
+        // peek O(1)
+        public static int peek() {
+            if(isEmpty()){
+                System.out.println("Queue is empty");
+                return -1;
+            }
+            return arr[front];
+        }
+    }
+
+    // Queue using linkedList
+        public static class Node{
+        int data;
+        Node next;
+        public Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+
+    static class LinkedListQueue{
+        static int size;
+        static Node head=null;
+        static Node tail=null;
+
+        // isEmpty
+        public static boolean isEmpty(){
+            return head==null&&tail==null;
+        }
+        
+        // add O(1)
+        public static void add(int data) {
+            Node newNode=new Node(data);
+            if(head==null){
+                head=tail=newNode;
+                size++;
+                return;
+            }
+            tail.next=newNode;
+            tail=newNode;
+            size++;
+        }
+        // remove O(1)
+        public static int remove() {
+            if(isEmpty()){
+                System.out.println("Queue already empty");
+                return -1;
+            }
+            int result=head.data;
+            // if there's only 1 element in the queue
+            if(head==tail){
+                head=tail=null;
+                size--;
+            }
+            else{
+                head=head.next;
+                size--;
+            }
+            return result;
+        }
+        // peek O(1)
+        public static int peek() {
+            if(isEmpty()){
+                System.out.println("Queue is empty");
+                return -1;
+            }
+            return head.data;
+        }
+    }
+    
+    public static void main(String[] args) {
+        // LinkedListQueue queue = new LinkedListQueue();
+        // Queue using JFC there are 2 ways to create a Queue :Using linkedList and lastlty using arrayDeque
+        // Queue<Integer> queue=new LinkedList<>();
+        Queue<Integer> queue=new ArrayDeque<>();
+        queue.add(10);
+        queue.add(20);
+        queue.add(30);
+        System.out.println(queue.remove());
+        queue.add(40);
+        System.out.println(queue.remove());
+        queue.add(50);
+        while(!queue.isEmpty()){
+            System.out.println(queue.remove());
+        }
+    }
+}
